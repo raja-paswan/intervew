44af1ff4cfd16c170130badc9d997b31d58c298f
Abhinavsssingh <singhabhinav22082000@gmail.com>
2022-12-19 16:05:59 +0530

    pakage

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,11 +1,16 @@
 const userModel = require("../Model/userModel")
 
-// const registerUser = async (req,res) =>{
-//     try{
-//         let data = req.body
-//         let image = req.files
-//         const{fname,lname,email,phone,password,address,}=data
-//         if(!fname) return res.status(400).send({})
+const registerUser = async (req, res) => {
+    try {
+        let data = req.body
+        let image = req.files
+        if (Object.keys(data).length == 0)
+            return res
+                .status(400)
+                .send({ status: false, message: "please give some data" });
+                
+        const { fname, lname, email, phone, password, address, } = data
+        if (!fname) return res.status(400).send({})
 
-//     }
-// }
+    }
+}
