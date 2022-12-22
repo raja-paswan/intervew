2a5851a9e22a986443262db4f245982d112398fd
gauravgd9719 <gd220310@gmail.com>
2022-12-22 12:40:39 +0530

    create cart api

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,3 +1,40 @@
 const userModel = require('../Model/userModel')
 const productModel = require('../Model/productModel')
 const cartModel = require('../Model/cartModel')
+
+
+const createCart = async function(req,res){
+    try{
+        const userId = req.params.userId
+        const tokenUserId = req.userId  //check
+        const cartData = req.body;
+
+        if(!userId){return res.status(400).send({status:false, message:`provide userId`})}
+
+        if(!isValidObjectId(userId)){return res.status(400).send({status:false,messsage:`userId Invalid`})}
+
+        if(!isValidObjectId(tokenUserId)){return res.status(400).send({status:false,message:`invalid tokenUserId`})}
+            
+        if(tokenUserId != userId){return res.status(403).send({status:false, message:`you are not authorised`})}
+
+        const user = await userModel.findOne({_id:userId})
+
+        if(!user){return res.status(404).send({status:false,message:`user not found`})}
+
+        if(!isValidRequestBody(cartData)){return res.status(400).send({status:false, message:`provide cart details`})}
+
+        const {productId, quantity, cartId, } = req.body;
+    
+        if(!isValidObjectId(productId)){return res.status(400).send({status:false,message:`invalid productId`})}
+
+        const checkProduct = await productModel.findOne({_id: productId, isDeleted:false})
+
+        if(!checkProduct){return res.status(404).send({status:false,message:`product not found`})}
+
+
+    }catch(error){
+        return res.status(500).send({status:false, messsage:error.messsage})
+    }
+}
+
+module.exports = {createCart}
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const MW = require('../middleware/auth')
 const{getProduct, getProductById , creatProduct , updateProduct , deleteProduct} = require("../controller/productController")
 const userController = require("../controller/userController")
+const cartController = require("../controller/cartController")
 //--------------------------------------------User Api--------------------------------------------------------------//
 
 router.post("/register",userController.registerUser)
@@ -18,6 +19,8 @@ router.get("/products",getProduct)
 router.put("/products/:productId",updateProduct)
 router.delete("/products/:productId", deleteProduct)
 
+//Cart API's
+router.post("/",MW.Authentication,cartController.createCart)
 
 
 
